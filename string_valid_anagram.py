c822a50e99c93448a323ca1266a43805866c3946
lwy2108 <88315275+lwy2108@users.noreply.github.com>
2022-09-12 01:29:11 +0800

    Add alternative solution for string_valid_anagram

--- string_valid_anagram.py
+++ string_valid_anagram.py
@@ -7,6 +7,17 @@ def string_valid_anagram(s, t):
   return sorted(s) == sorted(t)
 
 
+def string_valid_anagram(s, t):
+  if len(s) != len(t):
+    return False
+  else:
+    s_count, t_count = {}, {}
+    for i in range(len(s)):
+      s_count[s[i]] = 1 + s_count.get(s[i], 0)
+      t_count[t[i]] = 1 + t_count.get(t[i], 0)
+    return s_count == t_count
+
+
 # test cases
 s1, t1 = "anagram", "nagaram"
 s2, t2 = "rat", "car"
