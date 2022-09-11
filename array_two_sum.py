31a6b5395cbeb5ebecb21feabf93ac23c2f268a3
lwy2108 <88315275+lwy2108@users.noreply.github.com>
2022-09-12 01:51:17 +0800

    Solve array_two_sum

---
+++ array_two_sum.py
@@ -0,0 +1,25 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. There is exactly one solution.
+"""
+
+
+def array_two_sum(nums, target):
+  map = {}
+  for i in range(len(nums)):
+    diff = target - nums[i]
+    if diff in map:
+      return [map[diff], i]
+    else:
+      map[nums[i]] = i
+
+
+# test cases
+nums1 = [2,7,11,15]
+target1 = 9
+nums2 = [3,2,4]
+target2 = 6
+nums3 = [3,3]
+target3 = 6
+print(nums1, target1, "Pass" if array_two_sum(nums1, target1) == [0, 1] else "Fail")
+print(nums2, target2, "Pass" if array_two_sum(nums2, target2) == [1, 2] else "Fail")
+print(nums3, target3, "Pass" if array_two_sum(nums3, target3) == [0, 1] else "Fail")
