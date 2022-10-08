7f97e1d04393517e43197d87d4a4cc2d3d7a739c
Nicholas <88315275+lwy2108@users.noreply.github.com>
2022-10-08 13:55:32 +0800

    Improved efficiency of algorithm for dp_memo_can_sum

--- dp_memo_can_sum.py
+++ dp_memo_can_sum.py
@@ -15,6 +15,19 @@ def dp_memo_can_sum(target, nums):  # m = target, n = nums, T = O(n^m), S = O(m)
   return False
 
 
+def dp_memo_can_sum(target, nums, memo={}):  # m = target, n = nums, T = O(m*n), S = O(m)
+  if target in memo:
+    return memo[target]
+  if target == 0:
+    return True
+  for num in nums:
+    if num <= target and dp_memo_can_sum(target - num, nums, memo):
+      memo[target] = True
+      return True
+  memo[target] = False
+  return False
+
+
 # test cases
 target1, nums1 = 7, [2, 3]
 target2, nums2 = 7, [5, 3, 4, 7]
@@ -23,6 +36,6 @@ target4, nums4 = 8, [2, 3, 5]
 target5, nums5 = 300, [7, 14]
 print(target1, nums1, "Pass" if dp_memo_can_sum(target1, nums1) else "Fail")
 print(target2, nums2, "Pass" if dp_memo_can_sum(target2, nums2) else "Fail")
-print(target3, nums3, "Pass" if not dp_memo_can_sum(target3, nums3) else "Fail")
+print(target3, nums3, "Pass" if dp_memo_can_sum(target3, nums3) else "Fail")
 print(target4, nums4, "Pass" if dp_memo_can_sum(target4, nums4) else "Fail")
-print(target5, nums5, "Pass" if not dp_memo_can_sum(target5, nums5) else "Fail")
+print(target5, nums5, "Pass" if dp_memo_can_sum(target5, nums5) else "Fail")
